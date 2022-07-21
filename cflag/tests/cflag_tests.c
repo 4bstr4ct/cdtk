8265ba25bb230aa11815bf001a181d39f77064d5
Abstract <57039383+4bstr4ct@users.noreply.github.com>
2022-07-21 21:09:02 +0300

    ,

--- cflag/tests/cflag_tests.c
+++ cflag/tests/cflag_tests.c
@@ -6,31 +6,27 @@
 int main(int argc, char** argv)
 {
 	bool* boolFlag = _boolFlag(
-		"--boolFlag",
-		true,
-		"Enable tests"
+		"--boolFlag", "--boolFlag <value>",
+		true, "Enable tests"
 	);
 
 	int64* int64Flag = _int64Flag(
-		"--int64Flag",
-		0,
-		"Special value for some kind of thing"
+		"--int64Flag", "--int64Flag <value>",
+		0, "Special value for some kind of thing"
 	);
 
 	double* doubleFlag = _doubleFlag(
-		"--doubleFlag",
-		1,
-		"Doubly thing"
+		"--doubleFlag", "--doubleFlag <value>",
+		1, "Doubly thing"
 	);
 
 	const char* textFlag = _stringFlag(
-		"--textFlag",
-		"none",
-		"Some text for somtehing"
+		"--textFlag", "--textFlag <path>",
+		"none", "Some text for somtehing"
 	);
 
 	--argc; ++argv;
-	_parseFlags(argc, argv, PARSE_STRICT);
+	_parseFlags(argc, argv, PARSE_STRICT, "Usage: cflag_test.exe [OPTIONS]");
 
 	fprintf(stdout, "-------------------------\nboolFlag = %d\n", *boolFlag);
 	fprintf(stdout, "int64Flag = %lld\n", *int64Flag);
